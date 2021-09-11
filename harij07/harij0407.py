9ec7482248158d88ab5dca92bb8133eafbbb9285
Mohamed Mahmoud <mohamedmahmoud@Mohameds-ensin-MacBook-Pro.local>
2021-09-11 07:12:16 +0200

    calculus

---
+++ harij07/harij0407.py
@@ -0,0 +1,6 @@
+numero  = input("Anna kokonaisluku:")
+number = input("Anna toinen kokonaisluku:")
+print("Summa:", number + numero)
+print("Erotuksen:", number - numero)
+print("Tulon:", number*numero)
+print("Osamaaran:",number/numero)
