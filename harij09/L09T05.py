3efb0fdf0bd4c0bc5b13df8948f1e4d2c3033ff8
Mohamed <mohamed@Mohameds-Mac-mini.local>
2021-09-26 22:25:10 +0200

    fixed function issue

--- harij09/L09T05.py
+++ harij09/L09T05.py
@@ -1,17 +1,18 @@
 import random
-number_list = []
-number = 0
 
-new_number =0
-
-while len(number_list) <7:
-	new_number = random.randrange(0,40)
-	if new_number in number_list:
-		#number -=1
-		new_number =0
-	else:
-		number_list.append(new_number)
-		#number +=1
-
+def lotto():
+	new_number =0
+	number_list = []
+	number = 0
+	while len(number_list) <7:
+		new_number = random.randrange(0,40)
+		if new_number in number_list:
+			#number -=1
+			new_number =0
+		else:
+			number_list.append(new_number)
+			#number +=1
+	return number_list
+number_list = lotto()
 number_list.sort()
 print(number_list)
